a3ec1d3564ac1daf327a7e047acc07112ee95996
GuilleGarzon <guillegarzon.gg@gmail.com>
2022-08-15 09:19:27 -0500

    feat: fix file controller updateCard

--- api/cards/controller.js
+++ api/cards/controller.js
@@ -49,9 +49,9 @@ async function updateCardHandler(req, res) {
 
   try {
     const card = await updateCard(id, cardData);
-    return res.status(201).json(card)
+    return res.status(200).json({ message: 'Card updated' }, card)
   } catch (error) {
-    return res.status(500)-json({ error })
+    return res.status(500).json({ message: 'Error updating card' })
   }
 }
 
