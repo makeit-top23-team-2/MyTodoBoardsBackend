a469bf3a2955e9029473a6f6cf325320efb5d613
GuilleGarzon <guillegarzon.gg@gmail.com>
2022-08-15 09:06:01 -0500

    feat: add routes updateCards

--- api/cards/routes.js
+++ api/cards/routes.js
@@ -5,6 +5,7 @@ const {
   createCardHandler,
   getAllCardHandler,
   getSingleCardHandler,
+  updateCardHandler
 } = controller;
 
 const router = express.Router();
@@ -12,7 +13,7 @@ const router = express.Router();
 router.get('/', getAllCardHandler)
 router.post('/', createCardHandler)
 router.get('/:id', getSingleCardHandler)
-//router.patch('/:id', updateTaskHandler)
+router.patch('/:id', updateCardHandler)
 //router.delete('/:id', deleteTaskHandler)
 
 module.exports =  router ;
