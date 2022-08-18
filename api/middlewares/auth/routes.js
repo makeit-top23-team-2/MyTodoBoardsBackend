379b28fd75b628968bc4f9c8c882eab60c2e1639
Mauro Arango <cmauricio.arango@gmail.com>
2022-08-18 08:20:24 -0500

    delete repeated files

--- api/middlewares/auth/routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { loginUserHandler } = require('./services');
-const { validateLogin } = require('../../users/joiSchema');
-
-const router = Router();
-
-router.post('/login', validateLogin, loginUserHandler);
-
-module.exports = router;
