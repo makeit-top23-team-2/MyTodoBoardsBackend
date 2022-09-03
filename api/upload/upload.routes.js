9755a576f533b054254c585cf261f02a4a395891
Mauro Arango <91640454+ffyto@users.noreply.github.com>
2022-09-03 11:18:39 -0500

    Update upload.routes.js

--- api/upload/upload.routes.js
+++ api/upload/upload.routes.js
@@ -1,15 +1,14 @@
 const { Router } = require('express');
 const multer = require('multer');
-const { isAuthenticated } = require('../../auth/auth.services');
 
 const { uploadProfile, uploadBackgroundBoard, uploadBackgroundCard } = require('./upload.controller');
 
 const router = Router();
 const upload = multer({ dest: './temp' });
 
-router.post('/profile',  isAuthenticated,  upload.single('file'), uploadProfile)
-router.post('/background/board',  isAuthenticated, upload.single('file'), uploadBackgroundBoard)
-router.post('/background/card',  isAuthenticated, upload.single('file'), uploadBackgroundCard)
+router.post('/profile', upload.single('file'), uploadProfile)
+router.post('/background/board', upload.single('file'), uploadBackgroundBoard)
+router.post('/background/card', upload.single('file'), uploadBackgroundCard)
 
 
 
