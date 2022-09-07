4df3990dd41091d58f278c0eb7203becf3e16a5e
Mauro Arango <91640454+ffyto@users.noreply.github.com>
2022-09-07 10:37:17 -0500

    Feat/sending shared boards to user (#25)
    * adds functions to paint shared boards in the front
    
    * adjusting routes in user

--- api/users/users.routes.js
+++ api/users/users.routes.js
@@ -15,16 +15,18 @@ const {
   getUserByEmailHandler,
   findUserByUserNameHandler,
   updateUserHandler,
+  getAllSharedBoardsHandler,
 } = controller;
 
 const router = express.Router();
 
 router.get('/', getAllUserHandler);
+router.get('/shared-boards', isAuthenticated, getAllSharedBoardsHandler);
 router.get('/:id', getSingleUserHandler);
 router.get('/user/:userName', findUserByUserNameHandler);
 router.post('/', registerLogin, createUserHandler);
 router.get('/email/:email', getUserByEmailHandler);
-router.patch('/',  userUpdateValidation, isAuthenticated, updateUserHandler);
+router.patch('/', userUpdateValidation, isAuthenticated, updateUserHandler);
 router.delete('/', isAuthenticated, deleteUserHandler);
 
 module.exports = router;
